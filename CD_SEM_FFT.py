Fixed boundary rotation and line number labeling on b/w image

CD_SEM_FFT.py
@@ -109,7 +109,7 @@ def rotated_angle(probe: int, img: np.ndarray, lmax: int) -> float:
         ra = np.arctan((max_index - (probe - 3 + 1)) / (lmax / 2 - 1)) * 180 / np.pi
     else:
         ra = 0
-#    print("Angle of rotation:", ra)
+    #    print("Angle of rotation:", ra)
 
     """
     angle_range = np.linspace(-probe, probe, len(totals))
@@ -193,14 +193,11 @@ def fourier_pitch(img: object) -> float:
         # rewrite peakpositions in format: {"peak order No., peak position in nm^-1"}
         peakpositions = np.transpose([peakpositions, kx])
 
-        fitslope = np.polyfit(peakpositions[:, 0], peakpositions[:, 1], deg=1)
-        slope = fitslope[0]
-        intercept = fitslope[1]
+        slope, intercept = np.polyfit(peakpositions[:, 0], peakpositions[:, 1], deg=1)
 
         # fitpitch = 2 Pi/m /. fitslope
         fitpitch = 2 * np.pi / slope
 
-        """
         # Plotting
         plt.figure(figsize=(8, 6))
         plt.plot(
@@ -218,7 +215,7 @@ def fourier_pitch(img: object) -> float:
         plt.title(f"L0 = {round(fitpitch, 2)} nm")
         plt.legend()
         plt.show()
-        """
+
         return fitpitch
     else:
         raise ValueError(

CD_SEM_edges.py
@@ -68,7 +68,7 @@ def column_sums(img: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         np.ndarray: Sum of all the columns in the image
     """
     # Sum the columns along axis 0
-    column_sums = img.sum(axis=1)
+    column_sums = img.sum(axis=0)
     peaks = find_peaks(column_sums)[0]
 
     return (column_sums, peaks)
@@ -218,7 +218,7 @@ def trim_rotation(image: np.ndarray, angle_rad: float) -> np.ndarray:
     return trimmed_img
 
 
-def boundary_edges(boundary_img: np.ndarray) -> dict[str, list[tuple]]:
+def boundary_coords(boundary_img: np.ndarray) -> dict[str, list[tuple]]:
     """Takes the cleaned edge boundary image and returns a dictionary with with the coordinates
       of all the points making up a line is made and returned.
 
@@ -271,8 +271,8 @@ def rotate_edges(
         """
         return [
             (
-                (x - center[1]) * cos_theta - (y - center[0]) * sin_theta + center[1],
-                (x - center[1]) * sin_theta + (y - center[0]) * cos_theta + center[0],
+                ((x - center[1]) * sin_theta + (y - center[0]) * cos_theta) + center[0],
+                ((x - center[1]) * cos_theta - (y - center[0]) * sin_theta) + center[1],
             )
             for y, x in coordinates
         ]
@@ -283,26 +283,20 @@ def rotate_edges(
     }
 
     # Find the minimum and maximum y values among the rotated coordinates
-    min_y = max(
-        [
-            min(coord[0] for coord in rotated_coords)
-            for rotated_coords in rotated_dict.values()
-        ]
-    )
-    max_y = min(
-        [
-            max(coord[0] for coord in rotated_coords)
-            for rotated_coords in rotated_dict.values()
-        ]
-    )
+    min_y = max([coords[0][0] for coords in rotated_dict.values()])
 
-    # Trim the rotated coordinates to have the same length and starting/ending row coordinate
+    # Trim the rotated coordinates to have the same length and starting row coordinate
     for key in rotated_dict:
         rotated_coords = rotated_dict[key]
-        filtered_coords = [
-            coord for coord in rotated_coords if min_y <= coord[0] <= max_y
-        ]
+        filtered_coords = [coord for coord in rotated_coords if min_y <= coord[0]]
         rotated_dict[key] = filtered_coords
+
+    # Calculate the minimum length among all lists
+    min_length = min(len(coords) for coords in rotated_dict.values())
+
+    # Trim all lists to the minimum length
+    for key in rotated_dict:
+        rotated_dict[key] = rotated_dict[key][:min_length]
 
     return rotated_dict
 

CD_SEM_tools.py
@@ -15,7 +15,9 @@ def open_window():
     root.focus_force()
 
 
-def rescale_array(arr: np.ndarray, new_min: float = 0, new_max: float = 1) -> np.ndarray:
+def rescale_array(
+    arr: np.ndarray, new_min: float = 0, new_max: float = 1
+) -> np.ndarray:
     """Rescales the input array between the new_min and new_max values provided
 
     Args:
@@ -35,6 +37,7 @@ def rescale_array(arr: np.ndarray, new_min: float = 0, new_max: float = 1) -> np
         scaled_arr * (new_max - new_min) + new_min
     )  # Rescale to the new range
     return rescaled_arr
+
 
 def blackwhite_image(img: np.ndarray, midlevel: float) -> np.ndarray:
     """Takes and image and turns it into an array of 0's and 1's based on the input threshold
@@ -59,6 +62,7 @@ def blackwhite_image(img: np.ndarray, midlevel: float) -> np.ndarray:
     bw_image = (np.where(smoothed_image >= 0.5, 1, 0)).astype(np.uint8)
 
     return bw_image
+
 
 def clip_image(img: np.ndarray) -> np.ndarray:
     """Takes the input image and clips it between 0.05% and 99.95% of it origonal values.
@@ -93,7 +97,9 @@ def rotate_image(image: np.ndarray, angle: float) -> np.ndarray:
     return rotated_image
 
 
-def pad_vector_to_match_length(vector: np.ndarray, target_vector: np.ndarray) -> np.ndarray:
+def pad_vector_to_match_length(
+    vector: np.ndarray, target_vector: np.ndarray
+) -> np.ndarray:
     """Takes a vector and evenly added zeros to either end until it is the same
         length as the 'taget_vector'
 
@@ -168,6 +174,7 @@ def linear_fit(points: list) -> tuple[float, float]:
     m, c = np.linalg.lstsq(A, y, rcond=None)[0]
     return m, c
 
+
 def list_barplot(list: list | np.ndarray) -> None:
     """Displays a barplot of the provided list
 
@@ -185,3 +192,32 @@ def list_barplot(list: list | np.ndarray) -> None:
     plt.ylabel("Sum")
     plt.title("Sum of Image Lines")
     plt.show()
+
+
+def plot_peaks(peaks: list, scale: float) -> float:
+    """Displays the values of the peaks 'list' against the index of the value shifted to start at 1.
+
+    Args:
+        peaks (list): Pixel location of the peak of the column sum funtion
+        scale (float): size per pixel
+
+    Returns:
+        float: fitpitch to compare to previous values
+    """
+    indices = np.arange(1, len(peaks) + 1)  # Creating an array of indices
+    slope, intercept = np.polyfit(
+        indices, peaks, 1
+    )  # Fit a first-degree polynomial (line)
+
+    fitpitch = slope * scale
+
+    # Create a plot
+    plt.scatter(indices, peaks, label="List Values", color="gray")
+    plt.plot(indices, slope * indices + intercept, label="Fit")
+    plt.xlabel("Line No.")
+    plt.ylabel("Centroid, px")
+    plt.title(f"L0 = {round(fitpitch, 2)} nm, {len(peaks)} lines")
+    plt.legend()
+    plt.show()
+
+    return fitpitch

SEM_Image.py
@@ -42,7 +42,6 @@ class SEMImageDetails:
 
         self.peakposition: np.ndarray | None = None
         self.midlevel: float | None = None
-        self.column_sum: list | None = None
         self.grid_boundaries: dict | None = None
         self.rotated_boundaries: dict | None = (
             None  # Fractional coordinates of rotated image
@@ -89,8 +88,8 @@ class SEMImageDetails:
         # These operations have to deal with threasholding, binary filter
         self.midlevel = edges.threshold_level(self.image_flat, 0.6)
         self.image_binary = tools.blackwhite_image(self.image_flat, self.midlevel)
-            #tools.simple_image_display(self.image_binary, "Original Binary Image")
-        
+        # tools.simple_image_display(self.image_binary, "Original Binary Image")
+
         # Now removing defects and finding line edges
         self.image_boundaries = edges.remove_defects(np.copy(self.image_binary))
 
@@ -99,30 +98,26 @@ class SEMImageDetails:
         self.image_binary = edges.trim_rotation(
             tools.rotate_image(self.image_binary, self.rotate_angle), self.rotate_angle
         )
-        tools.simple_image_display(self.image_binary, "Straightend Binary Image")
 
         # Make boundary coordinate dictionary, rotate to fractional coordinates, trim to same begin and end row coordinates
-        self.rotated_boundaries = edges.boundary_edges(np.copy(self.image_boundaries))
+        self.rotated_boundaries = edges.boundary_coords(np.copy(self.image_boundaries))
         self.rotated_boundaries = edges.rotate_edges(
             self.rotated_boundaries, self.rotate_angle, (self.image_boundaries).shape
         )
 
         # Now that the binary image has been straightened we have to redo the line detection and line defect removal because the rotation of sigle pixel wide lines doesn't map well for visulization
         self.image_boundaries = edges.remove_defects(np.copy(self.image_binary))
-       
-        linesum, peaks = edges.column_sums(util.crop(np.copy(self.image_binary), ((5, 5), (5, 5))))
-        print(len(peaks))
-        tools.list_barplot(linesum)
-        
-        self.column_sum, peaks_boundaries = edges.column_sums(np.copy(self.image_boundaries))
-        print(len(peaks_boundaries))
-        tools.list_barplot(self.column_sum)
-        # tools.list_barplot(self.column_sum - test)
-        
-        #self.column_sum = edges.column_sums(edges.boundary_image(self.image_binary))
-        self.grid_boundaries = edges.boundary_edges(np.copy(self.image_boundaries))
-            #tools.simple_image_display(self.image_binary, "Straightend Binary")
-            #tools.simple_image_display(self.grid_boundaries, "Edge Boundaries")
+
+        linesum, peaks = edges.column_sums(
+            util.crop(np.copy(self.image_binary), ((5, 5), (5, 5)))
+        )
+
+        fitpitch_ceck = tools.plot_peaks(peaks, self.pix_scale * self.pix_size)
+        if abs(fitpitch_ceck - self.fitpitch) / self.fitpitch > 0.1:
+            raise ValueError(
+                f"Inconsistent Fitpitch after rotation and defect removal ({fitpitch_ceck} vs. {self.fitpitch}). Needs manually checked"
+            )
+
         edges.display_overlay(
             tools.blackwhite_image(np.copy(self.image_boundaries), 0.5),
             np.copy(self.image_binary),
@@ -133,6 +128,12 @@ class SEMImageDetails:
         self.bw_order = edges.edge_boundary_order(
             self.image_binary, self.grid_boundaries
         )
+        
+        # tools.list_barplot(linesum)
+
+        # self.column_sum = edges.column_sums(edges.boundary_image(self.image_binary))
+        self.grid_boundaries = edges.boundary_coords(np.copy(self.image_boundaries))
+
         # print(self.bw_order)
 
         ## NEEDS FIXED ###self.fitpitch = edges.pitch_fit(self.boundaries, (self.pix_size * self.pix_scale))
@@ -172,7 +173,7 @@ class SEMImageDetails:
         return metadata.pages[0].tags[tag_name].value
 
     def _pix_data(self, file_path: str, tag_name: str) -> tuple:
-        """Fetches the data associated with the image pixel dimensions from the library in the header. The multiplication of pixel scale and pixel size puts the pixel size in micrometers
+        """Fetches the data associated with the image pixel dimensions from the library in the header. The multiplication of pixel scale and pixel size puts the pixel size in nanometers
 
         Args:
             file_path (str): file path to the image